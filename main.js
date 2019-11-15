26e598ce05ee207c27f8fbe83722a7782e62d4f8
Yana Karcheuskaya <yana-03032017@mail.ru>
2019-11-15 09:27:00 +0300

    added new blocks

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
+let wrap = document.querySelector('.wrap');
+let fix = document.querySelector('.right-fix')
+let block_pos = fix.offsetTop; 
+let wrap_pos = wrap.offsetTop;
+let block_height = fix.offsetHeight;
+let wrap_height = wrap.offsetHeight;
+let blockwidth = fix.offsetWidth;
 
-block_pos = querySelector(".sidebar").offset().top;
-// определяем первоначальное положение блока
-wrap_pos = $('.add-ingredients').offset().top;
-// позиция контейнера
-block_height = $('.sidebar').outerHeight();
-// высота блока
-wrap_height = $('.wrap').outerHeight();
-// высота контейнера
-block_width = $('.sidebar').outerWidth();
-pos_absolute = wrap_pos + wrap_height - block_height;
-// ширина блока
+let pos_absolute = wrap_pos + wrap_height - block_height;
 
-window.scroll(function (){ 
-    if (window.scrollTop() > pos_absolute) {
-        querySelector(".sidebar").css({
-            'position': 'absolute',
-            'top': wrap_height - block_height,
-            'width': block_width
-        });
-
-    querySelector(".sidebar").css({
+window.addEventListener('scroll',function () {
+    if (window.pageYOffset > pos_absolute) {
+        fix.css({
             'position': 'absolute',
             'top': wrap_height - block_height,
             'width': block_width
         });
     }
-     else if (window.scrollTop() > block_pos) {
-// Если страницу прокрутили дальше, чем находится наш блок, то мы этот блок фиксируем и отображаем сверху
-        querySelector(".sidebar").css({
+    else if (window.pageYOffset > block_pos) {
+        fix.css({
             'position': 'fixed',
             'top': '0px',
             'width': block_width
         });
     } else {
-// Если же позиция скролла меньше (выше), чем наш блок, то возвращаем все назад
-    querySelector(".sidebar").css({
+        fix.css({
             'position': 'static'
         });
     }
-});
+})
