0869dca6e36a19deaf5c7e78da66084d69cb92c5
Yana Karcheuskaya <yana-03032017@mail.ru>
2019-11-16 10:46:00 +0300

    work with js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+//окно при нажатии на кнопку COOK
+let cookButton = document.querySelector('.cook-btn');
+let cookWindow = document.querySelector('.cook-window');
+let filter = document.querySelector('.filter');
+let closeButton = document.querySelector('.close');
 
+cookButton.addEventListener('click', function(){
+    cookWindow.style.display = 'block';
+    filter.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function(){
+    cookWindow.style.display = 'none';
+    filter.style.display = 'none';
+});
+
+//добавление продуктов в поле added products
+let ingredients = document.querySelectorAll('.left li');
+let productList = document.querySelector('.right-fix .added');
+
+ingredients.forEach(element => element.addEventListener('click', function(event){
+    let product = document.createElement('p');
+    product.innerHTML = `<b>${element.textContent}</b><br>  
+                        gr: <input id = "g">
+                        kcal: <output name = "kc">0
+                        $<output name = "pr">0`;
+    productList.append(product);
+}));
