4be16f9479f68c52fd31b3b5426c6c641bb1d8bd
Yana Karcheuskaya <yana-03032017@mail.ru>
2019-11-17 14:17:38 +0300

    work with list added products

--- main.js
+++ main.js
@@ -18,11 +18,32 @@ closeButton.addEventListener('click', function(){
 let ingredients = document.querySelectorAll('.left li');
 let productList = document.querySelector('.right-fix .added');
 
-ingredients.forEach(element => element.addEventListener('click', function(event){
+ingredients.forEach(element => element.addEventListener('click', function(){
+    //не добавлять, если уже в списке
+    let exist = document.querySelector('.added .'+ element.textContent)
+    if(exist){
+        productList.scrollTo(0, exist.offsetTop);
+        exist.style.backgroundColor = 'rgba(86, 97, 44, 0.7)';
+        setTimeout(() => exist.style.backgroundColor = 'rgba(243, 230, 113, 0.9)', 1000);
+        return;
+    }
+    
     let product = document.createElement('p');
+    product.classList.add(element.textContent)
     product.innerHTML = `<b>${element.textContent}</b><br>  
                         gr: <input id = "g">
                         kcal: <output name = "kc">0
-                        $<output name = "pr">0`;
+                        $<output name = "pr">0
+                        <div class="remove-product"></div>`;
     productList.append(product);
+    
+    productList.scrollBy(0, product.offsetTop);
 }));
+
+//при нажатии на крестик удалить из списка
+productList.addEventListener('click', function(event){
+    if (event.target.className != 'remove-product') 
+        return;
+    event.target.closest('p').remove(); 
+});
+
