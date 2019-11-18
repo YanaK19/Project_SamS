db33b07b9b1918faf1242630b41571310f14a5bb
Yana Karcheuskaya <yana-03032017@mail.ru>
2019-11-18 11:04:05 +0300

    db, fill ul, added hint

--- main.js
+++ main.js
@@ -1,3 +1,79 @@
+
+let ingredientsArr = [
+  {
+    id: 1,
+    group: "protein",
+    name: "chicken",
+    price100gr: 0.7,
+    kcal: 190,
+    pc: 1500
+  },
+  {
+    id: 2,
+    group: "protein",
+    name: "eggs",
+    price100gr: 0.3,
+    kcal: 157,
+    pc: 55
+  },
+  {
+    id: 3,
+    group: "protein",
+    name: "fish",
+    price100gr: 0.8,
+    kcal: 140
+  },
+  {
+    id: 4,
+    group: "protein",
+    name: "shrimps",
+    price100gr: 1.2,
+    kcal: 95,
+  },
+  {
+    id: 5,
+    group: "greenVeg",
+    name: "lettuce",
+    price100gr: 0.2,
+    kcal: 14,
+  },
+  {
+    id: 6,
+    group: "greenVeg",
+    name: "peas",
+    price100gr: 0.5,
+    kcal: 42,
+    pc:135
+  },
+  {
+    id: 7,
+    group: "greenVeg",
+    name: "broccoli",
+    price100gr: 0.5,
+    kcal: 28
+  },
+];
+
+//заполнение ul
+let proteinList = document.querySelector('.left .protein');
+let greenVegList = document.querySelector('.left .greenVeg');
+function fillUl(arr){
+  arr.forEach(function(item){
+    let liElement = document.createElement('li');
+    liElement.innerHTML = item.name;
+    switch(item.group){
+      case 'protein': 
+        proteinList.append(liElement);
+        break;
+      case 'greenVeg': 
+        greenVegList.append(liElement);
+        break;
+    }
+  });
+}
+
+fillUl(ingredientsArr);
+
 //окно при нажатии на кнопку COOK
 let cookButton = document.querySelector('.cook-btn');
 let cookWindow = document.querySelector('.cook-window');
@@ -5,6 +81,8 @@ let filter = document.querySelector('.filter');
 let closeButton = document.querySelector('.close');
 
 cookButton.addEventListener('click', function(){
+    //показать в окне весь заказ
+
     cookWindow.style.display = 'block';
     filter.style.display = 'block';
 });
@@ -27,14 +105,23 @@ ingredients.forEach(element => element.addEventListener('click', function(){
         setTimeout(() => exist.style.backgroundColor = 'rgba(243, 230, 113, 0.9)', 1000);
         return;
     }
-    
+
+    //найти объект с таким именем в БД
+    let elOfArr = ingredientsArr.find(el => el.name == element.textContent);
     let product = document.createElement('p');
-    product.classList.add(element.textContent)
-    product.innerHTML = `<b>${element.textContent}</b><br>  
-                        gr: <input id = "g" data-tooltip="1pc">
-                        kcal: <output name = "kc">0
-                        $<output name = "pr">0
-                        <div class="remove-product"></div>`;
+    product.classList.add(element.textContent);
+    let contentProduct = ``;
+    //если продукт поштучный, добавить подсказку
+    if(elOfArr.hasOwnProperty('pc'))
+      contentProduct += `data-tooltip="1pc-${elOfArr.pc}gr"`;
+
+    contentProduct =  `<b>${element.textContent}</b><br>  
+    gr: <input id = "g" `+ contentProduct + `>
+    kcal: <output name = "kc">0
+    $<output name = "pr">0
+    <div class="remove-product"></div>`
+
+    product.innerHTML = contentProduct;
     productList.append(product);
     
     productList.scrollBy(0, product.offsetTop);
